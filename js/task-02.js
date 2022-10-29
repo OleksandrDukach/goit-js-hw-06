301e4aec0d7caa1e2c7eeaf04db4f2bbcb0fc643
OleksandrDukach <umnick36@gmail.com>
2022-10-29 12:06:25 +0300

    1

--- js/task-02.js
+++ js/task-02.js
@@ -9,26 +9,26 @@ const ingredients = [
 
 
 //===============First method (old)==============
-const itemEl = document.querySelector("#ingredients");
+// const itemEl = document.querySelector("#ingredients");
 
-const elements = [];
-for (let i = 0; i < ingredients.length; i += 1) {
-  const option = ingredients[i];
-  const listEl = document.createElement('li');
-  listEl.textContent = ingredients[i];
-  elements.push(listEl);
-}
-console.log(elements)
+// const elements = [];
+// for (let i = 0; i < ingredients.length; i += 1) {
+//   const option = ingredients[i];
+//   const listEl = document.createElement('li');
+//   listEl.textContent = ingredients[i];
+//   elements.push(listEl);
+// }
+// console.log(elements)
 
-itemEl.append(...elements)
+// itemEl.append(...elements)
 
 //===============New method (якщо вийде (мод7 зан13(1:39:00)))==============
 
-// const itemEl = document.querySelector("#ingredients");
-// const elements = ingredients.map(option => {
-//   // const option = ingredients;
-//   const listEl = document.createElement('li');
-//   listEl.textContent = ingredients;
-//   // elements.push(listEl);
-//   return itemEl
-// });
+const itemEl = document.querySelector("#ingredients");
+const elements = ingredients.map(option => {
+  const option = ingredients;
+  const listEl = document.createElement('li');
+  listEl.textContent = ingredients;
+  elements.push(listEl);
+  return itemEl
+});
