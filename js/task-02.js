b2c00ead070636491512315126ce85aeb643b3e2
OleksandrDukach <umnick36@gmail.com>
2023-06-09 11:37:59 +0300

    New start

--- js/task-02.js
+++ js/task-02.js
@@ -1,3 +1,11 @@
+/* Напиши скрипт, который для каждого элемента массива ingredients:
+
+Создаст отдельный элемент <li>. Обзательно используй метод document.createElement().
+Добавит название ингредиента как его текстовое содержимое.
+Добавит элементу класс item.
+После чего вставит все <li> за одну операцию в список ul#ingredients. */
+
+
 const ingredients = [
   'Potatoes',
   'Mushrooms',
@@ -9,26 +17,26 @@ const ingredients = [
 
 
 //===============First method (old)==============
-// const itemEl = document.querySelector("#ingredients");
+const itemEl = document.querySelector("#ingredients");
 
-// const elements = [];
-// for (let i = 0; i < ingredients.length; i += 1) {
-//   const option = ingredients[i];
-//   const listEl = document.createElement('li');
-//   listEl.textContent = ingredients[i];
-//   elements.push(listEl);
-// }
-// console.log(elements)
+const elements = [];
+for (let i = 0; i < ingredients.length; i += 1) {
+  const option = ingredients[i];
+  const listEl = document.createElement('li');
+  listEl.textContent = ingredients[i];
+  elements.push(listEl);
+}
+console.log(elements)
 
-// itemEl.append(...elements)
+itemEl.append(...elements)
 
 //===============New method (якщо вийде (мод7 зан13(1:39:00)))==============
 
-const itemEl = document.querySelector("#ingredients");
-const elements = ingredients.map(option => {
-  const option = ingredients;
-  const listEl = document.createElement('li');
-  listEl.textContent = ingredients;
-  elements.push(listEl);
-  return itemEl
-});
+// const itemEl = document.querySelector("#ingredients");
+// const elements = ingredients.map(option => {
+//   const option = ingredients;
+//   const listEl = document.createElement('li');
+//   listEl.textContent = ingredients;
+//   elements.push(listEl);
+//   return itemEl
+// });
